4a3e4f53486b1e3ba12ff1937f2cda70f378648e
James <847798512@qq.com>
2020-06-15 16:50:07 +0800

    row 和 col 完成雏形

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .col {
+        height: 100px;
+        background: grey;
+        width: 50%;
+        border: 1px solid red;
+
+        $class: col-;
+        @for $n from 1 through 24{
+            &.#{$class}#{$n}{
+                width: ($n/24) * 100%;
+            }
+        }
+    }
+</style>
+<script>
+    export default {
+        name:'PureCol',
+        props:{
+            span:{type:[Number,String]}
+        }
+    }
+</script>
