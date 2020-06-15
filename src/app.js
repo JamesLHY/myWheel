b225c8d8dac3ef7badd0577ac0ba88925531736b
James <847798512@qq.com>
2020-06-15 14:45:15 +0800

    让input支持v-model双向绑定

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ Vue.component('g-input',Input )
 new Vue({
     el: '#app',
     data:{
-        loading1:false
+        loading1:false,
+        message:'hi'
     },
     methods:{
         inputChange(e){
