fdec39ebfa7248020a9f224f97e80b07e5b47b77
James <847798512@qq.com>
2020-06-16 17:25:33 +0800

    初步完成响应式

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="col" :class="colClass" :style='colStyle'>
-            <slot></slot>
-        </div>
+        <slot></slot>
+    </div>
 </template>
-
 <style lang="scss" scoped>
     .col {
         width: 50%;
@@ -19,14 +18,96 @@
                 margin-left: ($n/24) * 100%;
             }
         }
+        @media (min-width: 577px) and (max-width: 768px) {
+            $class: col-ipad-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    width: ($n/24) * 100%;
+                }
+            }
+            $class: offset-ipad-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    margin-left: ($n/24) * 100%;
+                }
+            }
+        }
+        @media (min-width: 769px) and (max-width: 992px) {
+            $class: col-narrow-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    width: ($n/24) * 100%;
+                }
+            }
+            $class: offset-narrow-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    margin-left: ($n/24) * 100%;
+                }
+            }
+        }
+        @media (min-width: 993px) and (max-width: 1200px) {
+            $class: col-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    width: ($n/24) * 100%;
+                }
+            }
+            $class: offset-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    margin-left: ($n/24) * 100%;
+                }
+            }
+        }
+        @media (min-width: 1201px) {
+            $class: col-wide-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    width: ($n/24) * 100%;
+                }
+            }
+            $class: offset-wide-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    margin-left: ($n/24) * 100%;
+                }
+            }
+        }
     }
 </style>
 <script>
+    let validator = (value) => {
+        let keys = Object.keys(value)
+        let valid = true
+        keys.forEach(key => {
+            if (!['span', 'offset'].includes(key)) {
+                valid = false
+            }
+        })
+        return valid
+    }
     export default {
         name: 'PureCol',
         props: {
             span: {type: [Number, String]},
             offset: {type: [Number, String]},
+            ipad: {
+                type: Object,
+                validator,
+            },
+            narrowPc: {
+                type: Object,
+                validator,
+            },
+            pc: {
+                type: Object,
+                validator,
+            },
+            widePc: {
+                type: Object,
+                validator,
+            },
         },
         data() {
             return {
@@ -35,10 +116,15 @@
         },
         computed: {
             colClass() {
-                let {span, offset} = this
+                let {span, offset,  ipad, narrowPc, pc, widePc} = this
+
                 return [
                     span && `col-${span}`,
-                    offset && `offset-${offset}`
+                    offset && `offset-${offset}`,
+                    ...(ipad && [`col-ipad-${ipad.span}`]),
+                    ...(narrowPc && [`col-narrowPc-${narrowPc.span}`]),
+                    ...(pc && [`col-pc-${pc.span}`]),
+                    ...(widePc && [`col-widePc-${widePc.span}`])
                 ]
             },
             colStyle() {
